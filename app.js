8a027f75a4c72de5951da27b4502b9bf66b0ebb0
Suryanshushah99 <56834606+Suryanshushah99@users.noreply.github.com>
2021-06-02 18:49:10 +0530

    Update app.js
    url update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-suryanshu:mydidig399@cluster0.ctny6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://admin-suryanshu:mydidig399@cluster0.ctny6.mongodb.net/tododb", {
   useNewUrlParser: true
 });
 
